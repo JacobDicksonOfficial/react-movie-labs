9f9f38a2b65c3e79c1885ef36c5fa65006a41458
Jacob <jjacobdofficial@gmail.com>
2024-10-04 16:02:22 +0100

    List of movies displayed.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,17 +1,17 @@
 import React from "react";
+import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid2";
-import MovieCard from '../components/movieCard'
 
 const HomePage = (props) => {
   const movies = props.movies;
-    
+
   return (
-  <Grid container>
+    <Grid container>
       <Grid size={12}>
-          <h1> HomePage </h1>
+        <h1> HomePage </h1>
       </Grid>
-      <Grid size={3}>
-        <MovieCard movie={movies[0]} />
+      <Grid container>
+        <MovieList movies={movies}></MovieList>
       </Grid>
     </Grid>
   );
