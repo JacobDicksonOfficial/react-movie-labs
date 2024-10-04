91aea21f0aa4b8100085d709d581e6d6d7f506bf
Jacob <jjacobdofficial@gmail.com>
2024-10-04 16:28:08 +0100

    Filter card and Header components added

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,30 +1,35 @@
 import React, { useState, useEffect } from "react";  
 import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid2";
+import Header from '../components/headerMovieList';
+import FilterCard from "../components/filterMoviesCard";
 
 const HomePage = (props) => {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-      fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-      )
-        .then((res) => res.json())
-        .then((json) => {
-          // console.log(json);
-          return json.results;
-        })
-        .then((movies) => {
-          setMovies(movies);
-        });
-    }, []);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, []);
 
   return (
     <Grid container>
       <Grid size={12}>
-        <h1> HomePage </h1>
+        <Header title={"Home Page"} />
       </Grid>
       <Grid container>
+        <Grid key="find" size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
+          <FilterCard />
+        </Grid>
         <MovieList movies={movies}></MovieList>
       </Grid>
     </Grid>
