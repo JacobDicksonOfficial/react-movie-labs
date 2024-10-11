e966930a41e8af90373f9e5eb9478550e0ae1330
Jacob <jjacobdofficial@gmail.com>
2024-10-11 15:41:23 +0100

    Change site header.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,32 +1,27 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";  // Removed Link from imports
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader';  // Added import for SiteHeader
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favorites">Favorites</Link>
-        </li>
-      </ul>
+      {/* Replaced <ul> navigation with SiteHeader */}
+      <SiteHeader />
       <Routes>
-        <Route exact path="/movies/favorites" element={<FavoriteMoviesPage />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
+        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
       </Routes>
     </BrowserRouter>
   );
 };
 
-const rootElement = createRoot( document.getElementById("root") )
+const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
